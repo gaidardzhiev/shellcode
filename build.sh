a004eb4e52708982e5416929c7c2a53012d15776
1v4n <42386354+gaidardzhiev@users.noreply.github.com>
2023-09-24 18:49:46 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ then
 	ld -N arm_rev_sh.o -o arm_rev_sh
 	objcopy -O binary arm_rev_sh arm_rev_sh.bin
 	hexdump -v -e '"\\""x" 1/1 "%02x" ""' arm_rev_sh.bin > shellcode
-elif [ $(uname -m) = x86_64];
+elif [ $(uname -m) = x86_64 ];
 then
 nasm -f elf32 spawn_shell.asm -o spawn_shell.o
 ld -m elf_i386 spawn_shell.o -o spawn_shell
